9e8b63659d0522fc54645b4ad64828e45ce15459
phumin2550 <phumintrakulpradit@gmail.com>
2024-06-08 21:13:39 +0700

    เสร็จจริงๆ

--- bmi.js
+++ bmi.js
@@ -1,29 +1,27 @@
-let weight = prompt("ส่วนสูง");
-let height = prompt("น้ำหนัก");
+bmi = () => {
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+    let para = document.getElementById("para");
 
-while(weight < 1){
-    weight = prompt("กรอกส่วนสูงใหม่");
+    if(weight < 1 || height < 1){
+        para.innerHTML = "กรุณากรอกข้อมูลให้ถูกต้อง";
+    }else{
+        let bmi1 = weight / (height * height);
+        let bmi2 = bmi1.toFixed(1);
+        let res;
+
+        if(bmi2 < 18.5){
+            res = "ผอมเกินไป";
+        }else if(bmi2 >= 18.5 && bmi2 <= 25.0){
+            res = "น้ำหนักปกติ";
+        }else if(bmi2 >= 25.0 && bmi2 <= 30.0){
+            res = "เริ่มอ้วน";
+        }else if(bmi2 > 30.0 && bmi2 <= 35.0){
+            res = "อ้วน";
+        }else if(bmi2 > 35.0){
+            res = "อ้วนมาก";
+        }
+
+        para.innerHTML = "bmi: "+bmi2+" "+res;
+    }
 }
-
-while(height < 1){
-    height = prompt("กรอกน้ำหนักใหม่");
-}
-
-let bmi1 = weight / (height * height);
-let bmi2 = bmi1.toFixed(1)
-let para = document.getElementById("para");
-let res;
-
-if(bmi2 < 18.5){
-    res = "ผอมเกินไป";
-}else if(bmi2 >= 18.5 && bmi2 <= 25.0){
-    res = "น้ำหนักปกติ";
-}else if(bmi2 >= 25.0 && bmi2 <= 30.0){
-    res = "เริ่มอ้วน";
-}else if(bmi2 > 30.0 && bmi2 <= 35.0){
-    res = "อ้วน";
-}else if(bmi2 > 35.0){
-    res = "อ้วนมาก";
-}
-
-para.innerHTML = "bmi: "+bmi2+" "+res;
